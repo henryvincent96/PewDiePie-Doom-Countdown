cc30b57f39fbb36d790d19c9d5dd66bb42a66843
Henry Vincent <henryvincent33@gmail.com>
2018-10-10 11:59:48 +0100

    Put counter on its own thread

--- pewdiepieDownfall.py
+++ pewdiepieDownfall.py
@@ -2,7 +2,9 @@ import urllib.request
 import json
 import time
 import platform
+import threading
 from os import system
+from appJar import gui
 
 def setClearScreenCommand():
     thisPlatform = platform.system()
@@ -31,32 +33,53 @@ key = 'AIzaSyBR7ZQmh02ETze1hjNS7rFYsSJSBYoUsvY'
 pdpID = 'UC-lHJZR3Gqxm24_Vd_AJ5Yw'
 tseriesID = 'UCq-Fj5jknLsUf-MWSy4_brA'
 
-#init
-clearScreenCommand = setClearScreenCommand()
-pdpCount = 0
-tseriesCount = 0
-netChange = 0
-previousCount = 0
-newCount = getCountdown()
+exitFlag = 0
 
-#main loop
-while True:
-    #logic
-    try:
-        previousCount = newCount
-        newCount = getCountdown()
-        netChange = newCount - previousCount
+class Counter (threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+    def run(self):
+        print('Starting ' + self.name)
+        startCounter(self.name, 5, self.counter)
+        print('Exiting ' + self.name)
 
-        if netChange >= 0:
-            netChange = str('+' + str(netChange))
-        #output
-        _ = system(clearScreenCommand)
-        print('PewDiePie Sub Count: ' + '{:,}'.format(pdpCount))
-        print('T Series Sub Count: ' + '{:,}'.format(tseriesCount))
-        print('Current Difference: ' + '{:,}'.format(newCount)+ ' (' + str(netChange) +')')
-    except TimeoutError:
-        print('Timeout error')
-    except urllib.error.URLError:
-        print('URL error')
-    #don't really want google thinking I'm DDOSing them
-    time.sleep(1)
+def startCounter(threadName, counter, delay):
+    #init
+    clearScreenCommand = setClearScreenCommand()
+    pdpCount = 0
+    tseriesCount = 0
+    netChange = 0
+    previousCount = 0
+    newCount = getCountdown()
+
+    #main loop
+    while True:
+        #logic
+        try:
+            if(exitFlag):
+                threadName.exit()
+
+            previousCount = newCount
+            newCount = getCountdown()
+            netChange = newCount - previousCount
+
+            if netChange >= 0:
+                netChange = str('+' + str(netChange))
+            #output
+            _ = system(clearScreenCommand)
+            print('PewDiePie Sub Count: ' + '{:,}'.format(pdpCount))
+            print('T Series Sub Count: ' + '{:,}'.format(tseriesCount))
+            print('Current Difference: ' + '{:,}'.format(newCount)+ ' (' + str(netChange) +')')
+        except TimeoutError:
+            print('Timeout error')
+        except urllib.error.URLError:
+            print('URL error')
+        #don't really want google thinking I'm DDOSing them
+        time.sleep(delay)
+
+counterThread = Counter(1, "CounterThread", 1)
+counterThread.start()
+print('Exiting main thread')
